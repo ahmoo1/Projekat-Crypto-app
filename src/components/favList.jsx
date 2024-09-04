f2ec19e622a6ad3146c452cc85568fdc2b171023
Junus07 <akjunus007@gmail.com>
2024-09-04 21:29:37 +0200

    Dodan Page za kad nema favorite coin-ova u listi

--- src/components/favList.jsx
+++ src/components/favList.jsx
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
 import { favContext } from "../context/FavContext";
 import FavItem from "./favItem";
+import NoFavs from "../images/No-Favs.png";
+import "./coins.css";
 
 const FavList = () => {
     const {favItems} = useContext(favContext);
 
     return(
         <div>
-            {favItems.map((coin) => (
+            {favItems.length !== 0 ? <div> {favItems.map((coin) => (
                 <div className="coincontainer">
                 <div className="coin" key={coin.uuid}>
                     <img className="coinimg" src={coin.iconUrl} alt="" />
@@ -19,7 +21,11 @@ const FavList = () => {
                     <FavItem className="heart" coin = {coin}/>
                 </div>
             </div>
-        ))}
+        ))} </div> : <div className="NoFavsContainer"> {<div>
+                <img src={NoFavs} alt="" />
+            <h1>No coins added to the favorites inventory</h1>
+            </div>} </div>
+            }
         </div>
     );
 }
