8e1239bd7d18f263111da6ec0fd77d3b74182990
ammardolovac <ammardolovac@gmail.com>
2024-09-04 17:43:12 +0200

    about us

---
+++ src/components/aboutus/aboutus.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './aboutus.css';
+
+const AboutUs = () => {
+  const teamMembers = [
+    {
+      name: 'Ammar Dolovac',
+      role: 'Software Developer',
+      image: '', 
+    },
+    {
+      name: 'Ahmed Avdic',
+      role: 'UI/UX Designer',
+      image: '',
+    },
+    {
+      name: 'Junus Osmanakic',
+      role: 'Frontend Developer',
+      image: '',
+    },
+    {
+      name: 'Dzemil Sahovic',
+      role: 'Backend Developer',
+      image: '',
+    },
+  ];
+
+  return (
+    <div className="aboutus-container">
+      {teamMembers.map((member) => (
+        <div className="card" key={member.name}>
+          <img src={member.image} alt={member.name} className="card-img" />
+          <h3 className="card-name">{member.name}</h3>
+          <p className="card-role">{member.role}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AboutUs;
