073052612f2e1ce809da067ec222512815c33349
ahmoo1 <ahmedavdic175@gmail.com>
2024-09-07 18:56:00 +0200

    Bug fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import TopTen from '../components/TopTen'
 import Header from '../components/header'
-import Footer from '../components/footer'
+
 
 const Home = () => {
   return (
     <>
     <Header />
     <TopTen />
-    <Footer />
+    
     </>
   )
 }
